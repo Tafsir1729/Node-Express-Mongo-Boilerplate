43af744fea1849cb9760e020083e8747f3577707
Tafsir Ahammed <Tafsir.Ahammed@liveimplevista.onmicrosoft.com>
2022-09-25 17:38:33 +0600

    protected routing middlewares

--- src/utils/protected.js
+++ src/utils/protected.js
@@ -15,4 +15,60 @@ const verifyToken = async (token) => {
  }
 };
 
+const isUser = async (req, res, next) => {
+ if (req.headers.authorization) {
+  try {
+   const user = await verifyToken(
+    req.headers.authorization.split("Bearer ")[1]
+   );
+   if (user) {
+    req.user = user;
+    next();
+   } else {
+    let msg = "Not Authenticated";
+    return response(res, StatusCodes.BAD_REQUEST, false, {}, msg);
+   }
+  } catch (error) {
+   return response(
+    res,
+    StatusCodes.INTERNAL_SERVER_ERROR,
+    false,
+    {},
+    error.message
+   );
+  }
+ } else {
+  let msg = "Authentication Token not found";
+  return response(res, StatusCodes.NOT_ACCEPTABLE, false, {}, msg);
+ }
+};
+
+const isAdmin = async (req, res, next) => {
+ if (req.headers.authorization) {
+  try {
+   const user = await verifyToken(
+    req.headers.authorization.split("Bearer ")[1]
+   );
+   if (user && user.userType === "admin") {
+    req.user = user;
+    next();
+   } else {
+    let msg = "Not Authenticated";
+    return response(res, StatusCodes.BAD_REQUEST, false, {}, msg);
+   }
+  } catch (error) {
+   return response(
+    res,
+    StatusCodes.INTERNAL_SERVER_ERROR,
+    false,
+    {},
+    error.message
+   );
+  }
+ } else {
+  let msg = "Authentication Token not found";
+  return response(res, StatusCodes.NOT_ACCEPTABLE, false, {}, msg);
+ }
+};
+
 module.exports = { verifyToken };
