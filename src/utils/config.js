82ddb8487300a05336341d60f983d2c450c64ed6
Tafsir Ahammed <Tafsir.Ahammed@liveimplevista.onmicrosoft.com>
2022-09-25 14:41:18 +0600

    create jwt token

---
+++ src/utils/config.js
@@ -0,0 +1,24 @@
+const { sign } = require("jsonwebtoken");
+const config = {
+ secrets: {
+  jwt: "PJaHvt8ASQvFgSgYI2gyc8a9TdHzLh5Rx98s7aB4nhUz4rvW92zsKvN6zbPIub",
+  jwtExp: "30d",
+ },
+};
+
+const createToken = (user) => {
+ return sign(
+  {
+   _id: user._id,
+   email: user.email,
+   name: user.name,
+   userType: user.userType,
+  },
+  config.secrets.jwt,
+  {
+   expiresIn: config.secrets.jwtExp,
+  }
+ );
+};
+
+module.exports = { createToken };
