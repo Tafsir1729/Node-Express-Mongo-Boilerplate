ec40f5b6008c51b6187df412e9313ba2d982763c
Tafsir Ahamed <tafsir15-9143@diu.edu.bd>
2022-10-06 15:58:14 +0600

    changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
- blobUrl: "https://ivsitestorage.blob.core.windows.net/sitecontent/",
- blobConnStr:
-  "DefaultEndpointsProtocol=https;AccountName=ivsitestorage;AccountKey=vgmAESLcEJCC3jbdpx/jAFfAe2icxmnH3rGidyZWyfA+SGcrKir/Jb70J2B+BXw8AQNpBtPIWZKs8/8IR690vQ==;EndpointSuffix=core.windows.net",
- containerName: "sitecontent",
+ blobUrl: "",
+ blobConnStr: "",
+ containerName: "",
 };
